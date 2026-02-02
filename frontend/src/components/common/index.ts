f5acb54d51f23aa649b4c30b54081dcb42e5644a
Sanjay <ssundaram@berkeley.edu>
2026-02-02 04:01:26 -0800

    feat(frontend): Export all common components from index

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Common Components Export
+ */
+
+export { Button } from './Button';
+export { Slider } from './Slider';
+export { Select } from './Select';
+export { Card, CardSection } from './Card';
+
